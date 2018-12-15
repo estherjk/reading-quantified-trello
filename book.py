22609487040d8fb1ee1d3ded196a2197b826b390
Esther Jun Kim <hello@esthermakes.tech>
2018-12-15 14:45:13 -0800

    Add Genre & GenreEndpoint classes
    This takes the Trello board's labels and converts them into a Genre object.

--- book.py
+++ book.py
@@ -20,7 +20,7 @@ class Book(object):
             'date_finished': self.date_finished,
             'genres': self.genres
         }
-        
+  
 
 class BookEndpoints(object):
     """
@@ -43,3 +43,32 @@ class BookEndpoints(object):
 
     def post_book(self, book):
         return self.client.post(self.path, book.to_json())
+
+
+class Genre(object):
+    """
+    Class representing a Genre object.
+    """
+    def __init__(self, name, trello_id):
+        self.name = name
+        self.trello_id = trello_id
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'trello_id': self.trello_id
+        }
+
+
+class GenreEndpoints(object):
+    """
+    Class for accessing the genre API endpoints.
+    """
+
+    path = '/api/genres/'
+
+    def __init__(self, client):
+        self.client = client
+
+    def post_genre(self, genre):
+        return self.client.post(self.path, genre.to_json())
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import parse
 import time
 
-from book import Book, BookEndpoints
+from book import Book, BookEndpoints, Genre, GenreEndpoints
 from client import ReadingQuantifiedClient
 from dotenv import load_dotenv
 from trello import TrelloClient
@@ -16,14 +16,27 @@ trello_client = TrelloClient(
 )
 finished_list = trello_client.get_list(constants.FINISHED_LIST_ID)
 cards_in_finished_list = finished_list.list_cards(actions=['createCard', 'updateCard:idList'])
+board = trello_client.get_board(constants.BOARD_ID)
+labels = board.get_labels()
 
 # Reading Quantified Client
 reading_quantified_client = ReadingQuantifiedClient(os.getenv('USERNAME'), os.getenv('PASSWORD'))
 book_endpoints = BookEndpoints(reading_quantified_client)
+genre_endpoints = GenreEndpoints(reading_quantified_client)
+
+# Add label as Genre item
+for label in labels:
+    print(label)
+    genre = Genre(
+        label.name,
+        label.id
+    )
+    response = genre_endpoints.post_genre(genre)
+    print(response)
+    time.sleep(2)
 
 # For each item in the Finished List, add them to Reading Quantified
 # TODO: Check if a card has already been added
-# TODO: Add genres
 for card in cards_in_finished_list:
     print(card)
     book = Book(
