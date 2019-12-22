66aa34c209a38c01e4b121785337e44fa54620e1
Esther Jun Kim <hello@esthermakes.tech>
2019-12-22 10:51:05 -0800

    WIP Get cover attachment info
    Still TODO: Updating an existing book on the Reading Quantified Server.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from trello import TrelloClient
 import os
 import time
 
-# This project's imports
+# ## This project's imports
 
 from books.book import Book, BookEndpoints, Genre, GenreEndpoints
 from books.client import ReadingQuantifiedClient
@@ -14,16 +14,23 @@ import books.parse as parse
 
 load_dotenv()
 
-# Trello Client
+# ## Trello Books board
+
 trello_client = TrelloClient(
     api_key=os.getenv('TRELLO_API_KEY')
 )
 finished_list = trello_client.get_list(constants.FINISHED_LIST_ID)
-cards_in_finished_list = finished_list.list_cards(actions=['createCard', 'updateCard:idList'])
 board = trello_client.get_board(constants.BOARD_ID)
 labels = board.get_labels()
 
-# Reading Quantified Client
+# Get the cards with just the necessary info...
+cards_in_finished_list = finished_list.list_cards(
+    actions=['createCard', 'updateCard:idList'],
+    query={'attachments': 'cover'}
+)
+
+# ## Reading Quantified Client
+
 reading_quantified_client = ReadingQuantifiedClient(os.getenv('BASE_URL'), os.getenv('USERNAME'), os.getenv('PASSWORD'))
 book_endpoints = BookEndpoints(reading_quantified_client)
 genre_endpoints = GenreEndpoints(reading_quantified_client)
@@ -34,7 +41,6 @@ trello_ids = [ genre['trello_id'] for genre in existing_genres ]
 
 # Add label as Genre item
 print('Adding new genres...')
-
 for label in labels:
     if label.id not in trello_ids:
         print(label)
@@ -44,30 +50,36 @@ for label in labels:
         )
         response = genre_endpoints.post_genre(genre)
         print(response)
-
 print('Done.')
 
 # Get all books that exist in Reading Quantified
 existing_books = book_endpoints.get_books()
 trello_ids = [ book['trello_id'] for book in existing_books ]
 
-# Add a book to Reading Quantified
+# Add books to Reading Quantified
 print('Adding new books...')
-
 for card in cards_in_finished_list:
     date_started = parse.get_date_started(card)
     date_finished = parse.get_date_finished(card)
 
-    if card.id not in trello_ids and date_started and date_finished:
+    # The cards have been filtered for just the cover attachment, but it's stored in a list.
+    cover_attachment = card.attachments[0] if card.attachments else {}
+    
+    if date_started and date_finished:
         print(card)
-        book = Book(
-            card.name,
-            card.id,
-            date_started,
-            date_finished,
-            genres=card.idLabels
-        )
-        response = book_endpoints.post_book(book)
-        print(response)
-
+        if card.id not in trello_ids:
+            print('Adding book...')
+            book = Book(
+                card.name,
+                card.id,
+                date_started,
+                date_finished,
+                genres=card.idLabels,
+                cover_attachment=cover_attachment
+            )
+            response = book_endpoints.post_book(book)
+            print(response)
+        else:
+            print('Updating book...')
+            # TODO: Add ability to update an existing book
 print('Done.')
