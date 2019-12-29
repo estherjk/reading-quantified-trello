61dc5a3eba8295917edcecf0389e015b8ff83f7b
Esther Jun Kim <hello@esthermakes.tech>
2019-12-29 10:08:21 -0800

    Refactor main.py
    Organized the code into 3 functions:
    
    * `sync_genres()`
    * `sync_books()`
    * `main()`
    
    TODO comments also added for updating existing genres & books.

--- main.py
+++ main.py
@@ -12,81 +12,92 @@ from books.models import Book, Genre
 import books.constants as constants
 import books.util as util
 
-load_dotenv()
+def sync_genres(trello_client, reading_quantified_client):
+    """
+    Synchronize the Reading Quantified Server with the genres (labels) listed in Trello.
+    """
 
-# ## Trello Books board
+    # Get labels from Trello
+    board = trello_client.get_board(constants.BOARD_ID)
+    labels = board.get_labels()
 
-trello_client = TrelloClient(
-    api_key=os.getenv('TRELLO_API_KEY')
-)
-finished_list = trello_client.get_list(constants.FINISHED_LIST_ID)
-board = trello_client.get_board(constants.BOARD_ID)
-labels = board.get_labels()
+    # Get all genres that exist on the Reading Quantified Server
+    existing_genres = reading_quantified_client.get_genres()
+    trello_ids = [ genre['trello_id'] for genre in existing_genres ]
 
-# Get the cards with just the necessary info...
-cards_in_finished_list = finished_list.list_cards(
-    actions=['createCard', 'updateCard:idList'],
-    query={'attachments': 'cover'}
-)
-
-# ## Reading Quantified Client
-
-reading_quantified_client = ReadingQuantifiedClient(os.getenv('BASE_URL'), os.getenv('USERNAME'), os.getenv('PASSWORD'))
-
-# Get all genres that exist in Reading Quantified
-existing_genres = reading_quantified_client.get_genres()
-trello_ids = [ genre['trello_id'] for genre in existing_genres ]
-
-# Add label as Genre item
-print('Adding new genres...')
-for label in labels:
-    if label.id not in trello_ids:
-        print(label)
-        genre = Genre(
-            label.name,
-            label.id,
-            color=label.color
-        )
-        response = reading_quantified_client.add_genre(genre)
-        print(response)
-print('Done.')
-
-# Get all books that exist in Reading Quantified
-existing_books = reading_quantified_client.get_books()
-trello_ids = [ book['trello_id'] for book in existing_books ]
-
-# Add books to Reading Quantified
-print('Adding new books...')
-for card in cards_in_finished_list:
-    date_started = util.get_date_started(card)
-    date_finished = util.get_date_finished(card)
-
-    # The cards have been filtered for just the cover attachment, but it's stored in a list.
-    cover_attachment = card.attachments[0] if card.attachments else {}
-
-    book = Book(
-        card.name,
-        card.id,
-        date_started,
-        date_finished,
-        genres=card.idLabels,
-        cover_attachment=cover_attachment
-    )
-    
-    if date_started and date_finished:
-        if card.id not in trello_ids:
-            print('Adding book...')
-            print(card)
-            response = reading_quantified_client.add_book(book)
+    print('Syncing genres...')
+    for label in labels:
+        if label.id in trello_ids:
+            # TODO: Update existing genre
+            pass
+        else:
+            print('Adding new genre...')
+            print(label)
+            genre = Genre(
+                label.name,
+                label.id,
+                color=label.color
+            )
+            response = reading_quantified_client.add_genre(genre)
             print(response)
-        # Comment out for now... queries the server a lot
-        # else:
-        #     # Existing books are missing cover attachment info... let's add it.
-        #     # TODO: Updating books should be more generic?
-        #     data = book_endpoints.get_books(query_params={'trello_id': card.id})
-        #     if not data[0]['cover_attachment'] and cover_attachment:
-        #         print('Updating book...')
-        #         response = book_endpoints.put_book(data[0]['url'], book)
-        #         print(response)
+    print('Done.')
 
-print('Done.')
+def sync_books(trello_client, reading_quantified_client):
+    """
+    Synchronize the Reading Quantified Server with the finished books (cards in Finished list) on Trello.
+    """
+    
+    # Get the cards with just the necessary info...
+    finished_list = trello_client.get_list(constants.FINISHED_LIST_ID)
+    cards_in_finished_list = finished_list.list_cards(
+        actions=['createCard', 'updateCard:idList'],
+        query={'attachments': 'cover'}
+    )
+
+    # Get all books that exist on the Reading Quantified Server
+    existing_books = reading_quantified_client.get_books()
+    trello_ids = [ book['trello_id'] for book in existing_books ]
+
+    print('Syncing books...')
+    for card in cards_in_finished_list:
+        date_started = util.get_date_started(card)
+        date_finished = util.get_date_finished(card)
+
+        # The cards have been filtered for just the cover attachment, but it's stored in a list.
+        cover_attachment = card.attachments[0] if card.attachments else {}
+
+        book = Book(
+            card.name,
+            card.id,
+            date_started,
+            date_finished,
+            genres=card.idLabels,
+            cover_attachment=cover_attachment
+        )
+        
+        if date_started and date_finished:
+            if card.id in trello_ids:
+                # TODO: Update existing book
+                pass
+            else:
+                print('Adding book...')
+                print(card)
+                response = reading_quantified_client.add_book(book)
+                print(response)
+    print('Done.')
+
+def main():
+    """
+    Main!
+    """
+    load_dotenv()
+    trello_client = TrelloClient(
+        api_key=os.getenv('TRELLO_API_KEY')
+    )
+    reading_quantified_client = ReadingQuantifiedClient(os.getenv('BASE_URL'), os.getenv('USERNAME'), os.getenv('PASSWORD'))
+
+    sync_genres(trello_client, reading_quantified_client)
+    sync_books(trello_client, reading_quantified_client)
+
+if __name__ == '__main__':
+    main()
